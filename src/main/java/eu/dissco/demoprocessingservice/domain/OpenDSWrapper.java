bcddef4ac859f1c42cd6d0f7646b4096a2a5d0a1
Sam Leeflang <sam.leeflang@naturalis.nl>
2022-02-17 13:37:21 +0100

    Fix property issue

--- src/main/java/eu/dissco/demoprocessingservice/domain/OpenDSWrapper.java
+++ src/main/java/eu/dissco/demoprocessingservice/domain/OpenDSWrapper.java
@@ -1,19 +1,17 @@
 package eu.dissco.demoprocessingservice.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Value;
-
 import java.util.List;
+import lombok.Data;
 
 @Data
 public class OpenDSWrapper {
 
-    @JsonProperty("ods:authoritative")
-    Authoritative authoritative;
-    @JsonProperty("ods:images")
-    List<Image> images;
-    String sourceId;
+  @JsonProperty("ods:authoritative")
+  Authoritative authoritative;
+  @JsonProperty("ods:images")
+  List<Image> images;
+  @JsonProperty("sourceId")
+  String sourceId;
 
 }
